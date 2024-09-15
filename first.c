61fa18e5b1dc7172b31488d2cca02a176a8bb10d
macbook <harisgorcevic1000@gmail.com>
2024-09-15 16:03:31 +0200

    first commit

---
+++ first.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     int num1, num2, sum;
+
+//     printf("Unesi prvi broj");
+//     scanf("%d", &num1);
+
+//     printf("Unesi drugi broj");
+//     scanf("%d", &num2);
+
+//     sum = num1 + num2;
+
+//     printf("Vas unos je : %d\n", sum);
+// }
+//==========================================================================================
+// 2. Zadatak
+// Napisati program koji će unositi ime i prezime korisnika i ispisati
+// ime i prezime korisnika u formatu "Ime prezime".
+//==========================================================================================
+// int main()
+// {
+//     char ime[20], prezime[20];
+//     printf("Unesi ime: ");
+//     scanf("%s", ime);
+//     printf("Unesi prezime: ");
+//     scanf("%s", prezime);
+//     printf("Vas unos je: %s %s\n", ime, prezime);
+// }
+//==========================================================================================
+// int main()
+// {
+//     int num1, num2, num3;
+//     int sum = 0;
+//     int count = 0;
+
+//     printf("Unesi prvi broj: ");
+//     scanf("%d", &num1);
+//     count++;
+
+//     printf("Unesi drugi broj: ");
+//     scanf("%d", &num2);
+//     count++;
+
+//     printf("Unesi treci broj :");
+//     scanf("%d", &num3);
+//     count++;
+
+//     sum = num1 + num2 + num3;
+
+//     printf("Aritmeticka sredina je %.2f\n", (float)sum / count);
+//     return 0;
+// }
+//===================================================================================================
