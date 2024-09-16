30edcbd3adee92880db7df9799b0c15a616b1ba9
macbook <harisgorcevic1000@gmail.com>
2024-09-16 18:29:35 +0200

    working on exercices

--- first.c
+++ first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
-// int main()
-// {
-//     int num1, num2, sum;
+// Zadatak 1
+//  int main()
+//  {
+//      int num1, num2, sum;
 
 //     printf("Unesi prvi broj");
 //     scanf("%d", &num1);
@@ -19,6 +19,7 @@
 // Napisati program koji će unositi ime i prezime korisnika i ispisati
 // ime i prezime korisnika u formatu "Ime prezime".
 //==========================================================================================
+// Zadatak 3
 // int main()
 // {
 //     char ime, prezime;
@@ -29,6 +30,7 @@
 //     printf("Vas unos je: %s %s\n", ime, prezime);
 // }
 //==========================================================================================
+// Zadatak 4
 // int main()
 // {
 //     int num1, num2, num3;
@@ -53,6 +55,7 @@
 //     return 0;
 // }
 //===========================================================================
+// Zadatak 5
 // int main()
 // {
 //     int i;
@@ -63,28 +66,79 @@
 //     return 0;
 // }
 //================================================================
-int main()
-{
-    int n;
-    printf("Unesite koliko zelite elemenata u niz da imate \n");
-    scanf("%d", &n);
+// Zadatak 6 Izabrati koliko zelite elemenata u niz prosek njihov i najveci u nizu koji je
+// int main()
+// {
+//     int n;
+//     printf("Unesite koliko zelite elemenata u niz da imate \n");
+//     scanf("%d", &n);
+//     int arr[n];
+//     int sum = 0;
+//     int max = 0;
 
-    int arr[n];
-    int sum = 0;
-    int max = 0;
+//     printf("Unesi %d celih brojeva:\n", n);
 
-    printf("Unesi %d celih brojeva:\n", n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Unesite vase brojeve: ");
+//         scanf("%d\n", &arr[i]);
+//         sum = sum + arr[i];
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+//     }
+//     printf("Prosek %d\n", sum / n);
+//     printf("Ovo je najveci broj %d\n", max);
+// }
+//=============================================================================================
+// Zadatak 6 ili proba
+// void func(int *num)
+// {
+//     *num = 10;
+// }
+// int main()
+// {
+//     int number = 5;
+//     int *a = &number;
+//     printf("%d\n", *a);
+//     func(a);
+//     printf("%d", *a);
+//     return 0;
+// }
+//==================================================================================================
+// Zadatak 7 sortiraj niz i printaj ga
+// int main()
+// {
+//     int n;
+//     printf("unesite koliko zelite brojeva u nizu \n");
+//     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("Unesite vase brojeve: ");
-        scanf("%d\n", &arr[i]);
-        sum = sum + arr[i];
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
-    }
-    printf("Prosek %d\n", sum / n);
-    printf("Ovo je najveci broj %d\n", max);
-}
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Unesite brojeve :\n");
+//         scanf("%d", &arr[i]);
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int y = 0; y < n; y++)
+//         {
+//             if (arr[i] > arr[y])
+//             {
+//                 int num = arr[i];
+//                 arr[i] = arr[y];
+//                 arr[y] = num;
+//             }
+//         }
+//     }
+//     printf("Sortirani niz je :");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d", arr[i]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+//==================================================================================================
